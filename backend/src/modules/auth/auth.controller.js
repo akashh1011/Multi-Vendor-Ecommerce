765acfaa927ea96f731e30a3df498b69f3cc0216
techux <connect.world12345@gmail.com>
2026-02-06 13:23:36 +0530

    feat: implement user registration and login with validation, add unique constraints to User model

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,18 +1,32 @@
+import expressAsyncHandler from "express-async-handler";
+import ApiError from "../../utils/ApiError.js";
 import ApiResponse from "../../utils/ApiResponse.js";
-import { loginService } from "./auth.service.js";
+import authService from "./auth.service.js";
+import authValidator from "./auth.validator.js";
 
-const loginController = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "Username and password are required" });
+const register = expressAsyncHandler(async (req, res) => {
+  const { error } = authValidator.register.validate(req.body);
+  if (error) {
+    throw new ApiError(400, error.details[0].message);
   }
 
-  const result = await loginService(req.body);
+  const result = await authService.register(req.body);
+
+  return ApiResponse(res, 201, "Registration Success", result);
+});
+
+const login = expressAsyncHandler(async (req, res) => {
+  const { error } = authValidator.login.validate(req.body);
+  if (error) {
+    throw new ApiError(400, error.details[0].message);
+  }
+
+  const result = await authService.login(req.body);
 
   return ApiResponse(res, 200, "Login Success", result);
-};
+});
 
-export default loginController;
+export default {
+  register,
+  login,
+};
--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,39 +1,81 @@
 import ApiError from "../../utils/ApiError.js";
-import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import {prisma} from "../../lib/prisma.js";
+import { prisma } from "../../config/prisma.js";
+import ENV from "../../config/envConfig.js";
 
-const loginService = expressAsyncHandler(async (credentials) => {
-  const { email, password } = credentials;
+const register = async (data) => {
+  const { name, email, phone, username, password, role } = data;
 
-  if (!email || !password) {
-    throw new ApiError(400, "Email and password are required");
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [
+        {
+          email,
+        },
+        {
+          username,
+        },
+        {
+          phone,
+        },
+      ],
+    },
+  });
+
+  if (existingUser) {
+    if (existingUser.email === email) {
+      throw new ApiError(400, "Email already in use");
+    }
+    if (existingUser.username === username) {
+      throw new ApiError(400, "Username already in use");
+    }
+    if (existingUser.phone === phone) {
+      throw new ApiError(400, "Phone number already in use");
+    }
   }
 
-  // Find user by email
-  const user = await prisma.user.findUnique({
-    where: { email },
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      phone,
+      username,
+      password: hashedPassword,
+      roles: [role || "CUSTOMER"],
+    },
+  });
+
+  return newUser.id;
+};
+
+const login = async (credentials) => {
+  const { emailOrUsername, password } = credentials;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: emailOrUsername }, { username: emailOrUsername }],
+    },
   });
 
   if (!user) {
     throw new ApiError(400, "Invalid username or password");
   }
 
-  // Compare passwords
   const isPasswordMatch = await bcrypt.compare(password, user.password);
 
   if (!isPasswordMatch) {
     throw new ApiError(400, "Invalid username or password");
   }
 
-  // Generate JWT token
   const token = jwt.sign(
     {
       id: user.id,
       email: user.email,
     },
-    process.env.JWT_SECRET,
+    ENV.JWT.SECRET,
     { expiresIn: "7d" }
   );
 
@@ -45,6 +87,6 @@ const loginService = expressAsyncHandler(async (credentials) => {
       name: user.name,
     },
   };
-});
+};
 
-export { loginService };
+export default { login, register };
